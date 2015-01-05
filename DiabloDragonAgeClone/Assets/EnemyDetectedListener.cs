0c2fe526bc876fdae6e46701c3fbfa0586184f42
Shrikkanth Sreedharan <shrikkanth.sreedharan@gmail.com>
2015-01-05 23:19:47 +1300

    Detection of ray expressed in terms of delegate/handler.  Smooth follow now in C sharp

---
+++ DiabloDragonAgeClone/Assets/EnemyDetectedListener.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDetectedListener : MonoBehaviour {
+
+
+	void OnEnable ()
+	{
+		EnemyDetector.enemyDetected += EnemyDetectedHandler;
+	}
+
+	void OnDisable ()
+	{
+		EnemyDetector.enemyDetected -= EnemyDetectedHandler;
+	}
+
+	public void EnemyDetectedHandler(Vector3 enemyPosition) {
+		Debug.Log ("Horse at: " + enemyPosition);
+	}
+	
+}
---
+++ DiabloDragonAgeClone/Assets/SmoothFollower.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollower : MonoBehaviour {
+
+	public Transform target;
+	// The distance in the x-z plane to the target
+	public float distance = 5.0f;
+	public float rotationDamping = 3.0f;
+
+	void LateUpdate () {
+		// Early out if we don't have a target
+		if (target == null)
+			return;
+
+		transform.position = target.position;
+		
+		Vector3 forwardFromCamera = transform.TransformDirection (Vector3.forward);
+		
+		RaycastHit hit;
+		
+		if (Physics.Raycast (transform.TransformPoint(0.5f * Vector3.right), forwardFromCamera, out hit)) {
+			if(hit.collider.gameObject.name.Contains("Horse")) {
+				transform.LookAt(hit.collider.bounds.center);
+			}
+		} else {	
+			// Always look at the target
+			transform.LookAt (target);
+		}
+		
+		// Calculate the current rotation angles
+		float wantedRotationAngle = target.eulerAngles.y;
+		
+		
+		float currentRotationAngleY = transform.eulerAngles.y;
+		float currentRotationAngleX = transform.eulerAngles.x;
+		
+		// Damp the rotation around the y-axis
+		currentRotationAngleY = Mathf.LerpAngle (currentRotationAngleY, wantedRotationAngle, rotationDamping * Time.deltaTime);
+		
+		// Damp the height
+		//currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+		
+		// Convert the angle into a rotation
+		Quaternion currentRotation = Quaternion.Euler (currentRotationAngleX, currentRotationAngleY, 0);
+		
+		Vector3 offsetBehindCharacter = new Vector3(0, 0, distance);
+		transform.position -= currentRotation * offsetBehindCharacter;
+	}
+}
